c39172c71d2c435f75c48288fcc2d3dbceb9123f
Chris <countinglampposts@gmail.com>
2025-05-30 09:31:38 -0600

    Added animation tweening

--- Assets/Scripts/AnimationBootstrap.cs
+++ Assets/Scripts/AnimationBootstrap.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Animancer;
 using UnityEngine;
 
@@ -7,12 +8,28 @@ namespace Jars.DevTest
     {
         [SerializeField] AnimancerComponent anim;
         [SerializeField] AnimationClip[] clips;
+        [SerializeField] AnimationClip idleClip;
+
+        Vector3 animBasePos;
+
+        private void Awake()
+        {
+            animBasePos = anim.transform.position;
+        }
 
         private void OnGUI()
         {
             foreach (var c in clips)
                 if (GUILayout.Button(c.name))
-                    anim.Play(c);
+                    StartCoroutine(TweenToAnimation(c));
+        }
+
+        IEnumerator TweenToAnimation(AnimationClip clip)
+        {
+            float fadeTime = 1f;
+            anim.Play(idleClip, fadeTime / 2);
+            yield return new WaitForSeconds(fadeTime / 2);
+            anim.Play(clip, fadeTime / 2);
         }
     }
 }
